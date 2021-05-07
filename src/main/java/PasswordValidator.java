805818097f980d4aa6281456591b3c8d7cb6fcfd
christianp <porsc.chr@gmail.com>
2021-05-07 20:17:33 +0200

    Add PasswordValidator to check for length and digits with tests

--- src/main/java/PasswordValidator.java
+++ src/main/java/PasswordValidator.java
@@ -1,2 +1,22 @@
 public class PasswordValidator {
+
+    public static boolean passwordLength(String password) {
+
+        if(password.length() < 5){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean passwordContainsDigits(String password) {
+
+        char[] checkForDigits = password.toCharArray();
+
+        for (int i = 0; i < checkForDigits.length; i++) {
+            if (Character.isDigit(password.charAt(i))){
+                return true;
+            }
+        }
+        return false;
+    }
 }
